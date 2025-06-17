91c80c353c0f91eb52c68dc07069098c8f4081f8
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-06-17 13:49:27 +0100

    Add admin area with role-based access

---
+++ Predictorator/Data/ApplicationDbInitializer.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Predictorator.Data;
+
+public static class ApplicationDbInitializer
+{
+    public static async Task SeedAdminUserAsync(IServiceProvider serviceProvider)
+    {
+        using var scope = serviceProvider.CreateScope();
+        var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+        var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+        const string adminRole = "Admin";
+        const string adminEmail = "admin@example.com";
+        const string adminPassword = "Admin123!";
+
+        if (!await roleManager.RoleExistsAsync(adminRole))
+        {
+            await roleManager.CreateAsync(new IdentityRole(adminRole));
+        }
+
+        var user = await userManager.FindByEmailAsync(adminEmail);
+        if (user == null)
+        {
+            user = new IdentityUser { UserName = adminEmail, Email = adminEmail, EmailConfirmed = true };
+            await userManager.CreateAsync(user, adminPassword);
+        }
+
+        if (!await userManager.IsInRoleAsync(user, adminRole))
+        {
+            await userManager.AddToRoleAsync(user, adminRole);
+        }
+    }
+}
