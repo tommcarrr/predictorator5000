abd3f539ec421516db5abfb57b3b962cd5356725
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-07-19 21:31:28 +0100

    Seed admin before scheduling

--- Predictorator/Program.cs
+++ Predictorator/Program.cs
@@ -178,6 +178,7 @@ app.MapGet("/logout", async (SignInManager<IdentityUser> sm) =>
 if (!app.Environment.IsEnvironment("Testing"))
 {
     app.UseHangfireDashboard();
+    await ApplicationDbInitializer.SeedAdminUserAsync(app.Services);
     RecurringJob.AddOrUpdate<NotificationService>(
         "fixture-notifications",
         s => s.CheckFixturesAsync(),
@@ -191,7 +192,6 @@ if (!app.Environment.IsEnvironment("Testing"))
         service => service.RemoveExpiredUnverifiedAsync(),
         "*/15 * * * *",
         new RecurringJobOptions());
-    await ApplicationDbInitializer.SeedAdminUserAsync(app.Services);
 }
 
 app.Run();
