9e125950584263f9015f2819f11c6f1cd7e40f46
Tom Carr <52778130+tommcarrr@users.noreply.github.com>
2025-08-12 11:19:26 +0100

    Add Azure Functions and shared core library

---
+++ Predictorator.Functions/FixtureNotificationsFunction.cs
@@ -0,0 +1,21 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Extensions.Timer;
+using Predictorator.Services;
+
+namespace Predictorator.Functions;
+
+public class FixtureNotificationsFunction
+{
+    private readonly NotificationService _service;
+
+    public FixtureNotificationsFunction(NotificationService service)
+    {
+        _service = service;
+    }
+
+    [Function("FixtureNotifications")]
+    public async Task Run([TimerTrigger("0 1 * * *")] TimerInfo timer)
+    {
+        await _service.CheckFixturesAsync();
+    }
+}
